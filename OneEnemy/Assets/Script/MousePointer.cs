46a9c57b4ac4dbaffe422fe7cfd7a8865ca30bb5
Haejoon0313 <sam960313@gmail.com>
2020-10-03 01:20:48 +0900

    Player Castle Guard Append & Land Material Append & Player Mouse Click Move & Dodge

---
+++ OneEnemy/Assets/Script/MousePointer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePointer : MonoBehaviour
+{
+    Vector3 destination;
+    MeshRenderer ren;
+
+    void Awake()
+    {
+        ren = GetComponent<MeshRenderer>();
+    }
+    void Update()
+    {
+        // right mouse input
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10000f))
+            {
+                destination = hit.point;
+                destination.y = 0.5f;
+            }
+
+            ren.enabled = true;
+            transform.position = destination;
+
+            Invoke("Inactive", 0.5f);
+        }
+        
+        
+    }
+    void Inactive()
+    {
+        ren.enabled = false;
+    }
+}
---
+++ OneEnemy/Assets/Script/Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 5;
+
+    bool dDown;
+    bool isDodge = false;
+
+    Vector3 destination = Vector3.zero;
+    Vector3 moveVec;
+    Vector3 dodgeVec;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+        Dodge();
+    }
+
+    void GetInput()
+    {
+        // right mouse input
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10000f))
+            {
+                destination = hit.point;
+
+                
+            }
+        }
+        destination.y = transform.position.y;
+
+        // dodge key input
+        dDown = Input.GetButtonDown("Dodge");
+    }
+
+    void Move()
+    {
+        if(Vector3.Distance(destination, transform.position) <= 0.01f)
+        {
+            anim.SetBool("isMove", false);
+            return;
+        }
+
+        moveVec = (destination - transform.position).normalized;
+
+        if (isDodge)
+            moveVec = dodgeVec;
+        
+        transform.position += moveVec * speed * Time.deltaTime;
+        anim.SetBool("isMove", true);
+    }
+
+    void Turn()
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    void Dodge()
+    {
+        if (dDown && !isDodge && moveVec != Vector3.zero)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 1f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+}
